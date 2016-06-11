9b1ec8da0f076f1a5616143d0af2c666aabc5cb1
theYounger <kuyehara1@gmail.com>
2016-06-10 15:22:43 -1000

    cash-register: second commit

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,9 +1,10 @@
 "use strict";
 
+//get elements/id by simple variable
 var content = document.getElementById('content');
 var dot = document.getElementById('.');
-var O = document.getElementById('0');
-var doubleO = document.getElementById('1');
+var zero = document.getElementById('0');
+var doubleZero = document.getElementById('00');
 var one = document.getElementById('1');
 var two = document.getElementById('2');
 var three = document.getElementById('3');
@@ -23,9 +24,42 @@ var getBalance = document.getElementById('get balance');
 var deposit = document.getElementById('deposit');
 var withdraw = document.getElementById('withdraw');
 
-O.addEventListener("click", load(0));
+//number variables
+var firstNum;
 
+//array
+var numArray = [];
 
+//turn elements of numArray into number; print number; return number
+var joinNumArray = function() {
+  firstNum = Number(numArray.join(''));
+  content.innerHTML = firstNum;
+  return firstNum;
+};
 
-content.innerHTML = ;
+zero.addEventListener("click", function() {numArray.push(0); joinNumArray();} );
+doubleZero.addEventListener("click", function() {numArray.push(0); numArray.push(0); joinNumArray();} );
+one.addEventListener("click", function() {numArray.push(1); joinNumArray();} );
+two.addEventListener("click", function() {numArray.push(2); joinNumArray();} );
+three.addEventListener("click", function() {numArray.push(3); joinNumArray();} );
+four.addEventListener("click", function() {numArray.push(4); joinNumArray();} );
+five.addEventListener("click", function() {numArray.push(5); joinNumArray();} );
+six.addEventListener("click", function() {numArray.push(6); joinNumArray();} );
+seven.addEventListener("click", function() {numArray.push(7); joinNumArray();} );
+eight.addEventListener("click", function() {numArray.push(8); joinNumArray();} );
+nine.addEventListener("click", function() {numArray.push(9); joinNumArray();} );;
+
+divOP.addEventListener("click", function() {
+
+} );;
+multOP.addEventListener("click", function() {
+
+} );;
+minusOP.addEventListener("click", function() {
+} );;
+addOP.addEventListener("click", function() {
+} );;
+equalOP.addEventListener("click", function() {
+
+} );;
 
